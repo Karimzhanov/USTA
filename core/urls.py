37ffff94663f2709b7f465bfd5478545624a83d7
Muhammadumar <karimzanovumar@gmail.com>
2024-10-15 17:04:45 +0600

    finish

--- core/urls.py
+++ core/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path('api/ambassadorship/', include('apps.ambassadorship.urls')),
     path('api/collaboration/', include('apps.collaboration.urls')),
 
-    
     path('swagger.<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
